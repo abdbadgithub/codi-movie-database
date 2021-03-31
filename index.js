c14ea5ba4cbd41f9713202c1d7028264fdc911d8
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-31 12:05:56 +0300

    step5

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 app.get("/", (req, res) => {
   res.send("ok");
 });
@@ -10,6 +15,14 @@ app.get("/test", (req, res) => {
 });
 app.get("/hello/:id", (req, res) => {
   res.send("hello " + req.params.id);
+});
+app.get("/time", (req, res) => {
+  let ts = new Date(Date.now());
+
+  // timestamp in milliseconds
+  let hours = ts.getHours();
+  let minutes = ts.getMinutes();
+  res.send({ status: 200, message: hours + ":" + minutes });
 });
 app.get("/search", (req, res) => {
   if (!req.query["s"]) {
@@ -22,15 +35,18 @@ app.get("/search", (req, res) => {
     res.send({ status: 200, message: "ok", data: req.query["s"] });
   }
 });
-app.get("/time", (req, res) => {
-  let ts = new Date(Date.now());
-
-  // timestamp in milliseconds
-  let hours = ts.getHours();
-  let minutes = ts.getMinutes();
-  res.send({ status: 200, message: hours + ":" + minutes });
+app.get("/movies/create", (req, res) => {
+  res.send({ status: 200, message: "ok" });
 });
-
+app.get("/movies/read", (req, res) => {
+  res.send({ status: 200, data: movies });
+});
+app.get("/movies/update", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/movies/delete", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
