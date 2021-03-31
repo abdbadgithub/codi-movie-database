cd679e6e74ae8c9745c3d2844d9e3b0b4a7aa616
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-31 13:45:40 +0300

    step6

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const movies = [
   { title: "Brazil", year: 1985, rating: 8 },
   { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
 ];
+const sortobject = (object, value) => {
+  object.sort((a, b) => a[value] - b[value]);
+  return object;
+};
 app.get("/", (req, res) => {
   res.send("ok");
 });
@@ -38,8 +42,18 @@ app.get("/search", (req, res) => {
 app.get("/movies/create", (req, res) => {
   res.send({ status: 200, message: "ok" });
 });
-app.get("/movies/read", (req, res) => {
-  res.send({ status: 200, data: movies });
+app.get("/movies/read/:input", (req, res) => {
+  if (req.params.input === "by-date") {
+    // res.send(req.params.input);
+    res.send({ status: 200, data: sortobject(movies, "year") });
+  }
+  if (req.params.input === "by-rating") {
+    res.send({ status: 200, data: sortobject(movies, "rating") });
+  }
+  if (req.params.input === "by-title") {
+    res.send({ status: 200, data: sortobject(movies, "title") });
+  }
+  //res.send({ status: 200, data: movies });
 });
 app.get("/movies/update", (req, res) => {
   res.send({ status: 200, message: "ok" });
